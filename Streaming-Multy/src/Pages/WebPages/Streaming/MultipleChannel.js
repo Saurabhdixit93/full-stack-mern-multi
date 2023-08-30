04df2d4e3d0745d1613c1b481089ce53933b234a
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2023-08-30 18:51:13 +0530

    Update MultipleChannel.js

--- Streaming-Multy/src/Pages/WebPages/Streaming/MultipleChannel.js
+++ Streaming-Multy/src/Pages/WebPages/Streaming/MultipleChannel.js
@@ -14,6 +14,7 @@ import {
 } from "@chakra-ui/react";
 import { FaChevronRight } from "react-icons/fa";
 import ResponsiveEmbed from "react-responsive-embed";
+
 const channels = {
   music: [
     {
@@ -21,8 +22,12 @@ const channels = {
       name: "9xm music",
     },
     {
-      url: "https://www.sonymix.tv/videos.php",
-      name: "Sony Mix",
+      url: "blob:https://www.watcho.com/206f5285-05ca-498a-bbdb-bca0b37e2d24",
+      name: "music India",
+    },
+    {
+      url: "blob:https://www.watcho.com/542229b8-9186-4f55-bee2-67b61ae42a5e",
+      name: "9x Jalva",
     },
   ],
   news: [
@@ -33,6 +38,61 @@ const channels = {
     {
       url: "https://feeds.intoday.in/livetv/ver-3-0/?id=livetv-it&amp;autostart=1",
       name: "India Today",
+    },
+    {
+      url: "https://www.youtube.com/embed/nyd-xznCpJc",
+      name: "Abp news",
+    },
+    {
+      url: "blob:https://www.watcho.com/f0b28588-5232-4df4-9962-a3c8cde207af",
+      name: "India Tv",
+    },
+    {
+      url: "blob:https://cdn.jwplayer.com/d5020922-1a77-48ce-9670-906960209095",
+      name: "India Tv 2",
+    },
+    {
+      url: "http://play4you.icu/e/46a3a514a5",
+      name: "News 24",
+    },
+  ],
+  Entertainment: [
+    {
+      url: "blob:https://www.watcho.com/2f2bae3f-b796-4bcf-b651-2ba74729cbf7",
+      name: "DD National",
+    },
+    {
+      url: "blob:https://www.yupptv.com/3ac18d37-fba7-4812-8e27-a10b850c345b",
+      name: "DD India",
+    },
+    {
+      url: "http://play4you.icu/e/1894c68b9d",
+      name: "Cartoon Network",
+    },
+    {
+      url: "http://desistreams.tv/embed/pogo.php",
+      name: "Pogo Tv",
+    },
+    {
+      url: "http://play4you.icu/e/ff37785958",
+      name: "B4U Plus",
+    },
+    {
+      url: "http://play4you.icu/e/06310c0bfb",
+      name: "B4U Movies",
+    },
+    {
+      url: "http://desistreams.tv/embed/sony_tv.php",
+      name: "Sony Tv",
+    },
+    {
+      url: "http://desistreams.tv/embed/star_plus.php'",
+      name: "Star Plus",
+    },
+
+    {
+      url: "http://desistreams.tv/embed/star_gold2.php",
+      name: "Star Gold",
     },
   ],
   cricket: [
@@ -47,12 +107,12 @@ const channels = {
   ],
 };
 
-// channel stream
 const StreamingPage = () => {
   const [currentSrcIndexes, setCurrentSrcIndexes] = useState({
     music: 0,
     news: 0,
     cricket: 0,
+    Entertainment: 0,
   });
 
   const iframeRef = useRef(null);
@@ -99,6 +159,7 @@ const StreamingPage = () => {
                   <Text fontSize="xl" fontWeight={"bold"}>
                     {channels[category][currentSrcIndexes[category]].name}
                   </Text>
+
                   <Box
                     bg="gray.100"
                     borderRadius="md"
@@ -107,12 +168,13 @@ const StreamingPage = () => {
                     width="100%"
                     maxW="800px"
                     position="relative"
+                    ref={iframeRef} // Add this ref
                   >
                     <ResponsiveEmbed
                       src={channels[category][currentSrcIndexes[category]].url}
                       allowFullScreen
                       ratio="16:9"
-                      onDoubleClick={() => handleDoubleClick(iframeRef.current)}
+                      onDoubleClick={handleDoubleClick}
                     />
                   </Box>
                   {channels[category].length > 1 && (
